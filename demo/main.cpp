a8b810cdbe3f7d35413d7eb0fd407c0564916ecc
Henning Erlandsen <henning.erlandsen@gmail.com>
2018-02-22 13:45:52 +0100

    Add sleep to main loop

--- demo/main.cpp
+++ demo/main.cpp
@@ -1,11 +1,18 @@
 #include "Window.h"
 
-int main(int argc, char** argv) {
+#include <thread>
+#include <chrono>
+
+using namespace std::chrono_literals;
+
+int main(int argc, char** argv) 
+{
 
     Window window(640, 480, "Hello, World!");
     
-    while(window.update()) {
-        
+    while(window.update()) 
+    {
+        std::this_thread::sleep_for(16ms);
     }
 
     return 0;
