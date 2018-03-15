177323068272f541789c8885dd8e25e8c7b94426
Henning Erlandsen <henning.erlandsen@gmail.com>
2018-03-15 22:37:11 +0100

    Store component

--- hildring/engine/include/EntityComponentSystem.h
+++ hildring/engine/include/EntityComponentSystem.h
@@ -6,13 +6,32 @@
 namespace ecs {
 
 class System {
+    class ComponentWrapperBase {
+    public:
+        virtual ~ComponentWrapperBase() = default;
+    };
+
+    template <class T>
+    class ComponentWrapper : public ComponentWrapperBase {
+    public:
+        ComponentWrapper(std::unique_ptr<T> component)
+            : component(std::move(component))
+        {
+        }
+
+    private:
+        std::unique_ptr<T> component;
+    };
+
 public:
-    template <typename ComponentHanderT, typename... Args>
+    template <typename ComponentT, typename... Args>
     void addComponentHandler(Args... args)
     {
-        std::make_unique<ComponentHanderT>(std::forward<Args>(args)...);
+        auto component = std::make_unique<ComponentT>(std::forward<Args>(args)...);
+        m_component = std::make_unique<ComponentWrapper<ComponentT>>(std::move(component));
     }
 
 private:
+    std::unique_ptr<ComponentWrapperBase> m_component;
 };
 }
--- hildring/engine/test/EntityComponentSystemTest.cpp
+++ hildring/engine/test/EntityComponentSystemTest.cpp
@@ -4,18 +4,38 @@
 
 TEST_CASE("Entity component system")
 {
-    struct Component {
-        Component(bool* createFlag)
-        {
-            *createFlag = true;
-        }
-    };
-
     SECTION("It creates component on registration")
     {
+        struct ComponentMock {
+            ComponentMock(bool* createFlag)
+            {
+                *createFlag = true;
+            }
+        };
+
         ecs::System system;
         bool created = false;
-        system.addComponentHandler<Component>(&created);
+        system.addComponentHandler<ComponentMock>(&created);
         REQUIRE(created);
+    }
+
+    SECTION("It keeps the component alive")
+    {
+        struct ComponentMock {
+            ComponentMock(bool* aliveFlag)
+                : aliveFlag(aliveFlag)
+            {
+            }
+
+            ~ComponentMock() { *aliveFlag = false; }
+
+            bool* aliveFlag;
+        };
+
+        ecs::System system;
+        bool componentAlive = true;
+        system.addComponentHandler<ComponentMock>(&componentAlive);
+
+        REQUIRE(componentAlive);
     }
 }
