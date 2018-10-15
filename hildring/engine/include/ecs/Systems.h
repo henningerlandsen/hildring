af0953397ff8af4b4009e52dc699c107390da358
Henning <henning.erlandsen@gmail.com>
2018-10-15 21:40:12 +0200

    Remove overly complicated systems registry (#18)
    * Make Systems a template class containing a system. Remove vector of Systems
    
    * Remove empty Systems.cpp
    
    * Remove unused includes
    
    * Add lifetime token to systems
    
    * Remove public reset method
    
    * Adapt systems tests to refactor
    
    * Add expiration test
    
    * Rename result to token
    
    * Remove unused method
    
    * Rename already taken variable name
    
    * Ignore tests for coverage report

--- hildring/engine/include/ecs/Systems.h
+++ hildring/engine/include/ecs/Systems.h
@@ -1,73 +1,48 @@
 #pragma once
 
-#include "util/Index.h"
+#include "util/LifetimeToken.h"
 
-#include <assert.h>
 #include <memory>
-#include <vector>
 
 namespace ecs {
+template <typename System>
 class Systems {
-    using SystemIndex = util::Index<unsigned long, ~0ul>;
-
-    template <class System>
-    class SystemMapping {
-    public:
-        static SystemIndex index;
-    };
-
 public:
-    template <typename System, typename... Args>
-    static bool create(Args&&... args)
+    template <typename... Args>
+    static auto create(Args&&... args)
     {
-        if (!SystemMapping<System>::index.valid()) {
-            SystemMapping<System>::index = createId();
-            systems.emplace_back(new System(std::forward<Args>(args)...), [](void* system) {
-                SystemMapping<System>::index.invalidate();
-                delete static_cast<System*>(system);
-            });
-            return true;
+        if (!valid()) {
+            system = std::make_unique<System>(std::forward<Args>(args)...);
+            return util::LifetimeToken([]() { Systems<System>::reset(); });
         }
-        return false;
+        return util::LifetimeToken();
     }
 
-    template <typename System, typename Accessor>
+    template <typename Accessor>
     static bool with(Accessor&& accessor)
     {
-        if (auto system = getSystem<System>()) {
+        if (valid()) {
             accessor(*system);
             return true;
         }
         return false;
     }
 
+private:
+    static bool valid()
+    {
+        return system != nullptr;
+    }
+
     static void reset()
     {
-        systems.clear();
+        system.reset();
     }
 
-private:
-    template <typename System>
-    static System* getSystem()
-    {
-        const auto index = SystemMapping<System>::index;
-        if (index.valid() && index < systems.size()) {
-            return static_cast<System*>(systems[index].get());
-        }
-        return nullptr;
-    }
-
-    static SystemIndex createId()
-    {
-        return SystemIndex{ systems.size() };
-    }
-
-    using SystemsContainer = std::vector<std::unique_ptr<void, void (*)(void*)>>;
-
-    static SystemsContainer systems;
+    static std::unique_ptr<System> system;
 };
 
-template <class System>
-Systems::SystemIndex Systems::SystemMapping<System>::index = Systems::SystemIndex();
+template <typename System>
+std::unique_ptr<System> Systems<System>::system;
 }
 
--- hildring/engine/test/ecs/ComponentsTest.cpp
+++ hildring/engine/test/ecs/ComponentsTest.cpp
@@ -91,7 +91,7 @@ SCENARIO("Registering components")
             {
                 const auto result = ecs::Components<Component>::with(
                     42,
-                    [](Component&) { assert(false); });
+                    [](Component&) { CHECK(false); });
                 THEN("accessing fails")
                 {
                     CHECK(result == false);
@@ -109,29 +109,29 @@ SCENARIO("Registering components")
 
             WHEN("System is created")
             {
-                ecs::Systems::create<System>();
+                auto token = ecs::Systems<System>::create();
 
                 THEN("Component can be created")
                 {
                     const auto createResult = ecs::Components<Component>::create(42);
                     CHECK(createResult);
                 }
-            }
 
-            WHEN("lifetime token is moved")
-            {
+                WHEN("lifetime token is moved")
                 {
-                    const auto linkResultCopy = std::move(linkResult);
-                    THEN("link still exists")
                     {
-                        CHECK(ecs::Components<Component>::create(42) == true);
+                        const auto linkResultCopy = std::move(linkResult);
+                        THEN("link still exists")
+                        {
+                            CHECK(ecs::Components<Component>::create(42) == true);
+                        }
                     }
-                }
-                WHEN("moved token is destroyed")
-                {
-                    THEN("link is broken")
+                    WHEN("moved token is destroyed")
                     {
-                        CHECK(ecs::Components<Component>::create(42) == false);
+                        THEN("link is broken")
+                        {
+                            CHECK(ecs::Components<Component>::create(42) == false);
+                        }
                     }
                 }
             }
@@ -140,7 +140,7 @@ SCENARIO("Registering components")
 
     GIVEN("System is created")
     {
-        ecs::Systems::create<System>();
+        auto token = ecs::Systems<System>::create();
 
         WHEN("linking a Component")
         {
@@ -161,7 +161,7 @@ SCENARIO("Registering components")
 
                 THEN("System creates it")
                 {
-                    ecs::Systems::with<System>([](System& system) {
+                    ecs::Systems<System>::with([](System& system) {
                         CHECK(system.createCalled);
                     });
                 }
@@ -181,7 +181,7 @@ SCENARIO("Registering components")
                     CHECK(ecs::Components<Component>::with(42, [](Component& c) {
                         c.name = "Dingo";
                     }));
-                    CHECK(ecs::Systems::with<System>([](System& s) {
+                    CHECK(ecs::Systems<System>::with([](System& s) {
                         CHECK(s.component.name == "Dingo");
                     }));
                 }
@@ -192,7 +192,7 @@ SCENARIO("Registering components")
 
                     THEN("Systems destroy is called")
                     {
-                        CHECK(ecs::Systems::with<System>([](System& s) {
+                        CHECK(ecs::Systems<System>::with([](System& s) {
                             CHECK(s.destroyCalled);
                         }));
                     }
@@ -223,7 +223,7 @@ SCENARIO("Registering components")
             bool destroy(const ecs::EntityId) { return false; }
         };
 
-        ecs::Systems::create<BadAllocSystem>();
+        auto token = ecs::Systems<BadAllocSystem>::create();
         const auto linkResult = ecs::Components<int>::link<BadAllocSystem>();
 
         WHEN("creating component")
