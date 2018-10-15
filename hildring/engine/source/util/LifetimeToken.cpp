ed5a49650ac43150c917c88408889a1a02e2c407
Henning Erlandsen <henning.erlandsen@gmail.com>
2018-10-15 22:30:34 +0200

    Add move assignment to LifetimeToken

--- hildring/engine/source/util/LifetimeToken.cpp
+++ hildring/engine/source/util/LifetimeToken.cpp
@@ -8,17 +8,30 @@ LifetimeToken::LifetimeToken(std::function<void()> expirationCallback)
 
 LifetimeToken::LifetimeToken() {}
 
+LifetimeToken::~LifetimeToken()
+{
+    if (callback) {
+        callback();
+    }
+}
+
 LifetimeToken::LifetimeToken(LifetimeToken&& other)
     : callback(std::move(other.callback))
 {
     other.callback = nullptr;
 }
 
-LifetimeToken::~LifetimeToken()
+LifetimeToken&
+LifetimeToken::operator=(LifetimeToken&& other)
 {
-    if (callback) {
-        callback();
+    if (this != &other) {
+        if (callback) {
+            callback();
+        }
+        callback = other.callback;
+        other.callback = nullptr;
     }
+    return *this;
 }
 
 LifetimeToken::operator bool() const
