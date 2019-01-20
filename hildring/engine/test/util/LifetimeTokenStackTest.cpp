30c311a080e4bafcc81c44d6b9c97e5f23af6990
Henning Erlandsen <henning.erlandsen@gmail.com>
2019-01-20 21:19:07 +0100

    Don't expect tokens to be destroyed in any specific order

--- hildring/engine/test/util/LifetimeTokenStackTest.cpp
+++ hildring/engine/test/util/LifetimeTokenStackTest.cpp
@@ -29,40 +29,25 @@ SCENARIO("Tokens can be pushed on the stack")
     }
 }
 
-SCENARIO("It invokes tokens in the reverse order of creation")
+SCENARIO("It invokes the tokens when stack is cleared")
 {
     GIVEN("A LifetimeTokenStack with two tokens")
     {
-        std::vector<int> destroyOrder;
+        auto destroyCount = 0u;
         auto tokenStack = util::LifetimeTokenStack{};
-        tokenStack.push(util::LifetimeToken([&destroyOrder]() {
-            destroyOrder.push_back(2);
-        }));
-        tokenStack.push(util::LifetimeToken([&destroyOrder]() {
-            destroyOrder.push_back(1);
-        }));
+        auto callback = [&destroyCount]() {
+            destroyCount++;
+        };
+        tokenStack.push(util::LifetimeToken(callback));
+        tokenStack.push(util::LifetimeToken(callback));
 
         WHEN("the stack is cleared")
         {
             tokenStack = util::LifetimeTokenStack{};
             THEN("the second token is destroyed first")
             {
-                CHECK(destroyOrder[0] == 1);
-                CHECK(destroyOrder[1] == 2);
+                CHECK(destroyCount == 2);
             }
         }
-    }
-
-    GIVEN("A vector of tokens")
-    {
-        bool one = false, two = false;
-        std::vector<util::LifetimeToken> tokens;
-        tokens.emplace_back([&one]() { one = true; });
-        tokens.emplace_back([&two]() { two = true; });
-
-        tokens.pop_back();
-
-        CHECK(two == true);
-        CHECK(one == false);
     }
 }
